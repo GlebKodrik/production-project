3d42d6c95305f8633a9fb2bedeeaf7e0feb69758
Gleb <kodrikgleb2@mail.ru>
2022-11-21 23:21:09 +0300

    refactoring

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
+import React, { Suspense } from 'react';
 import '@styles/global.scss';
 
-import React, { Suspense } from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
