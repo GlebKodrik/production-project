ac9224e5e2bb06e16e058a989b5c8cdffcfb1a4a
Gleb Kodrik <kodrikgleb2@mail.ru>
2023-01-06 23:42:05 +0300

    fixed store

--- src/providers/redux-provider/redux-provider.tsx
+++ src/providers/redux-provider/redux-provider.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import { createReduxStore } from 'redux-stores/root-store';
 import { Provider } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
 
 export const ReduxProvider: React.FC = ({ children }) => {
-  const navigation = useNavigate();
-  const rootStore = createReduxStore({ navigation });
+  const rootStore = createReduxStore({ });
+
   return (
     <Provider store={rootStore}>
       {children}
